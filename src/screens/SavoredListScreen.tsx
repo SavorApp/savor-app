89eec469511e50c9783ba90d014a7d15a4467db2
yoshiohasegawa <yoshio.seisuke.hasegawa@gmail.com>
2021-05-31 21:29:29 +0900

    Working on SavoredListScreen

--- src/screens/SavoredListScreen.tsx
+++ src/screens/SavoredListScreen.tsx
@@ -9,11 +9,10 @@ import {
   Text,
   TouchableOpacity,
 } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { SavoredListParamList } from "../../types";
+import { SavoredListParamList, RootState, UserRecipeListState } from "../../types";
 import { colorPalette, shadowStyle } from "../constants/ColorPalette";
-//Import for dev
-import * as recipesJson from "../data/recipes.json";
 
 const _screen = Dimensions.get("screen");
 
@@ -22,6 +21,9 @@ export interface SavoredListScreenProps {
 }
 
 export default function SavoredListScreen({ navigation }: SavoredListScreenProps) {
+  const userRecipeListState = useSelector<RootState, UserRecipeListState>((state) => state.userRecipeListState);
+
+
   const Item = ({ title }: any) => (
     <View style={styles.item}>
       <TouchableOpacity
@@ -29,41 +31,77 @@ export default function SavoredListScreen({ navigation }: SavoredListScreenProps
           navigation.navigate("RecipeScreen", { recipeId: "recipeID_12345" })
         }
       >
-        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.recipeTitle}>{title}</Text>
       </TouchableOpacity>
     </View>
   );
 
-  const DATA = recipesJson.recipes;
+
   const renderItem = ({ item }: any) => {
     return <Item title={item.title} key={item.id} />;
   };
 
   // TODO: Get Savored List from global state
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList data={DATA} renderItem={renderItem} />
-    </SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.subContainer}>
+          <View style={styles.contentContainer}>
+            <FlatList style={styles.flatList} contentContainerStyle={styles.flatListContainer} data={userRecipeListState.userRecipeList} renderItem={renderItem} />
+          </View>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: colorPalette.background,
   },
 
-  item: {
+  subContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: _screen.width * 0.9,
+    height: _screen.height * 0.75,
+    borderRadius: 30,
     backgroundColor: colorPalette.primary,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    ...shadowStyle,
+  },
+
+  contentContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: _screen.width * 0.86,
+    height: _screen.height * 0.73,
+    borderRadius: 30,
+    backgroundColor: colorPalette.secondary,
+  },
+
+  flatList: {
+    // padding: 8,
+    width: _screen.width * 0.83,
+    borderRadius: 30,
+    backgroundColor: colorPalette.secondary,
+  },
+
+  flatListContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  item: {
+    justifyContent: "center",
+    alignItems: "center",
+    // margin: 8,
+    width: _screen.width * 0.8,
     borderRadius: 10,
-    ...shadowStyle
+    backgroundColor: colorPalette.background
   },
   
-  title: {
-    fontSize: 32,
+  recipeTitle: {
+    fontSize: 16,
   },
 });
