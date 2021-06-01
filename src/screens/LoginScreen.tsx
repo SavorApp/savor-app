7d823893164749ba76d5cc4cb62c380edbdc64ac
yoshiohasegawa <yoshio.seisuke.hasegawa@gmail.com>
2021-06-01 13:46:13 +0900

    code syntax change

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -25,12 +25,14 @@ export interface LoginScreenProps {
 }
 
 export default function LoginScreen({ navigation }: LoginScreenProps) {
-    // useDispatch allows us to dispatch Actions to mutate global store variables
-    const dispatch = useDispatch();
-    const [userInput, setUserInput] = React.useState<InputUser>({username: "", password: ""})
-    const [validUsername, setValidUsername] = React.useState(false);
-    const [hidePassword, setHidePassword] = React.useState(true);
-
+  // useDispatch allows us to dispatch Actions to mutate global store variables
+  const dispatch = useDispatch();
+  const [userInput, setUserInput] = React.useState<InputUser>({
+    username: "",
+    password: "",
+  });
+  const [validUsername, setValidUsername] = React.useState(false);
+  const [hidePassword, setHidePassword] = React.useState(true);
 
   function usernameInputChange(val: string) {
     setUserInput({
@@ -45,19 +47,18 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
       password: val,
     });
   }
-  
+
   function handleHidePassword() {
     setHidePassword(!hidePassword);
   }
-  
-  React.useEffect(() => {
-      if (userInput.username.length >= 4) {
-          setValidUsername(true);
-      } else {
-          setValidUsername(false);
-      }
-  }, [userInput])
 
+  React.useEffect(() => {
+    if (userInput.username.length >= 4) {
+      setValidUsername(true);
+    } else {
+      setValidUsername(false);
+    }
+  }, [userInput]);
 
   function handleLogin(data: InputUser) {
     if (data.username.length < 4) {
@@ -96,213 +97,192 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
         })
         .catch((error: any) => {
           // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
+          const errorCode = error.code;
+          const errorMessage = error.message;
         });
     }
   }
 
   return (
-        <View style={styles.container}>
-            <View style={styles.subContainer}>
-                <Text style={styles.title}>Please Sign In</Text>
-                <View style={styles.form}>
-                    <View style={styles.inputContainer}>
-                        <View style={[styles.input, {justifyContent: "space-between"}]}>
-                            <View style={styles.passwordContainer}>
-                                <MaterialCommunityIcons
-                                    name="account-outline"
-                                    size={20}
-                                />
-                                <TextInput
-                                    style={{width: _screen.width*0.5}}
-                                    placeholder="Your Username"
-                                    autoCapitalize="none"
-                                    onChangeText={(val) => usernameInputChange(val)}
-                                />
-                            </View>
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <Text style={styles.title}>Please Sign In</Text>
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <View style={[styles.input, { justifyContent: "space-between" }]}>
+              <View style={styles.passwordContainer}>
+                <MaterialCommunityIcons name="account-outline" size={20} />
+                <TextInput
+                  style={{ width: _screen.width * 0.5 }}
+                  placeholder="Your Username"
+                  autoCapitalize="none"
+                  onChangeText={(val) => usernameInputChange(val)}
+                />
+              </View>
 
-                            {validUsername ? 
-                                <MaterialCommunityIcons 
-                                    name="checkbox-marked-circle-outline"
-                                    color="green"
-                                    size={20}
-                                />
-                                :
-                                <></>
-                            }
-                        </View>
-
-                        <View style={[styles.input, {justifyContent: "space-between"}]}>
-                            <View style={styles.passwordContainer}>
-                                <MaterialCommunityIcons
-                                    name="lock-outline"
-                                    size={20}
-                                />
-                                <TextInput
-                                    style={{width: _screen.width*0.5}}
-                                    placeholder="Your Password"
-                                    secureTextEntry={hidePassword ? true : false}
-                                    autoCapitalize="none"
-                                    onChangeText={(val) => passwordInputChange(val)}
-                                />
-                            </View>
-
-                                <TouchableOpacity
-                                    onPress={handleHidePassword}
-                                >
-                            {hidePassword ? 
-                                <MaterialCommunityIcons 
-                                    name="eye-off"
-                                    color="grey"
-                                    size={20}
-                                />
-                                :
-                                <MaterialCommunityIcons 
-                                    name="eye"
-                                    color="grey"
-                                    size={20}
-                                />
-                            }
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-
-                    <View style={styles.signInButtonContainer}>
-                        <TouchableOpacity
-                        onPress={() => {handleLogin(userInput)}}
-                        activeOpacity={0.8}
-                        >
-                            <LinearGradient
-                                colors={[colorPalette.alternate, colorPalette.alternate2]}
-                                style={styles.signUpButton}
-                            >
-                                <Text
-                                    style={{color: colorPalette.background}}
-                                >Sign In</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-                <View>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("SignupScreen")}
-                    >
-                        <Text
-                            style={styles.signUp}
-                        >Sign Up</Text>
-
-                    </TouchableOpacity>
-                </View>
-
-                <View>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("AboutUsScreen")}
-                        activeOpacity={0.8}
-                    >
-                        <LinearGradient
-                            colors={[colorPalette.popLight, colorPalette.popDark]}
-                            style={styles.aboutUsButton}
-                        >
-                            <Text
-                                style={{color: "black"}}
-                            >About Us</Text>
-                        </LinearGradient>
-
-                    </TouchableOpacity>
-                </View>
+              {validUsername ? (
+                <MaterialCommunityIcons
+                  name="checkbox-marked-circle-outline"
+                  color="green"
+                  size={20}
+                />
+              ) : (
+                <></>
+              )}
             </View>
+
+            <View style={[styles.input, { justifyContent: "space-between" }]}>
+              <View style={styles.passwordContainer}>
+                <MaterialCommunityIcons name="lock-outline" size={20} />
+                <TextInput
+                  style={{ width: _screen.width * 0.5 }}
+                  placeholder="Your Password"
+                  secureTextEntry={hidePassword ? true : false}
+                  autoCapitalize="none"
+                  onChangeText={(val) => passwordInputChange(val)}
+                />
+              </View>
+
+              <TouchableOpacity onPress={handleHidePassword}>
+                {hidePassword ? (
+                  <MaterialCommunityIcons
+                    name="eye-off"
+                    color="grey"
+                    size={20}
+                  />
+                ) : (
+                  <MaterialCommunityIcons name="eye" color="grey" size={20} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={styles.signInButtonContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                handleLogin(userInput);
+              }}
+              activeOpacity={0.8}
+            >
+              <LinearGradient
+                colors={[colorPalette.alternate, colorPalette.alternate2]}
+                style={styles.signUpButton}
+              >
+                <Text style={{ color: colorPalette.background }}>Sign In</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
         </View>
+
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate("SignupScreen")}>
+            <Text style={styles.signUp}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AboutUsScreen")}
+            activeOpacity={0.8}
+          >
+            <LinearGradient
+              colors={[colorPalette.popLight, colorPalette.popDark]}
+              style={styles.aboutUsButton}
+            >
+              <Text style={{ color: "black" }}>About Us</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colorPalette.background,
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: colorPalette.background
-    },
+  subContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: _screen.width * 0.9,
+    height: _screen.height * 0.6,
+    borderRadius: 30,
+    backgroundColor: colorPalette.primary,
+    ...shadowStyle,
+  },
 
-    subContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: _screen.width*0.9,
-        height: _screen.height*0.6,
-        borderRadius: 30,
-        backgroundColor: colorPalette.primary,
-        ...shadowStyle
-    },
+  form: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 48,
+    width: _screen.width * 0.8,
+    height: _screen.height * 0.3,
+    borderRadius: 30,
+    backgroundColor: colorPalette.secondary,
+  },
 
-    form: {
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 48,
-        width: _screen.width*0.8,
-        height: _screen.height*0.3,
-        borderRadius: 30,
-        backgroundColor: colorPalette.secondary
-    },
+  title: {
+    marginVertical: 8,
+    fontSize: 28,
+    fontWeight: "bold",
+    color: colorPalette.background,
+  },
 
-    title: {
-        marginVertical: 8,
-        fontSize: 28,
-        fontWeight: "bold",
-        color: colorPalette.background,
-      },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 1,
+    padding: 6,
+    paddingBottom: 18,
+    width: _screen.width * 0.7,
+    borderRadius: 10,
+    backgroundColor: colorPalette.background,
+  },
 
-    inputContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        margin: 1,
-        padding: 6,
-        paddingBottom: 18,
-        width: _screen.width * 0.7,
-        borderRadius: 10,
-        backgroundColor: colorPalette.background,
-    },
+  input: {
+    flexDirection: "row",
+    marginTop: 10,
+    paddingHorizontal: 3,
+    width: _screen.width * 0.65,
+    borderBottomWidth: 1,
+    borderBottomColor: colorPalette.trim,
+  },
 
-    input: {
-        flexDirection: "row",
-        marginTop: 10,
-        paddingHorizontal: 3,
-        width: _screen.width*0.65,
-        borderBottomWidth: 1,
-        borderBottomColor: colorPalette.trim,
-    },
+  passwordContainer: {
+    flexDirection: "row",
+  },
 
-    passwordContainer: {
-        flexDirection: "row"
-    },
+  signInButtonContainer: {
+    marginTop: 10,
+  },
 
-    signInButtonContainer: {
-        marginTop: 10
-    },
+  signUpButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    width: 200,
+    borderRadius: 10,
+    padding: 8,
+  },
 
-    signUpButton: {
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 10,
-        width: 200,
-        borderRadius: 10,
-        padding: 8
-    },
-    
-    signUp: {
-        margin: 8,
-        fontSize: 18,
-        textDecorationLine: "underline",
-        color: colorPalette.background
-    },
+  signUp: {
+    margin: 8,
+    fontSize: 18,
+    textDecorationLine: "underline",
+    color: colorPalette.background,
+  },
 
-    aboutUsButton: {
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 8,
-        width: 120,
-        borderRadius: 10,
-        padding: 8
-    }
-})
+  aboutUsButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+    width: 120,
+    borderRadius: 10,
+    padding: 8,
+  },
+});
