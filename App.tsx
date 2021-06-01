5d094c68b1828d518af13de66a7607b03a833e40
yoshiohasegawa <yoshio.seisuke.hasegawa@gmail.com>
2021-06-01 12:55:04 +0900

    Implemnted onStartFetchData() with Firebase Auth

--- App.tsx
+++ App.tsx
@@ -1,31 +1,14 @@
-import React, { useEffect } from "react";
-import { Provider, useDispatch } from "react-redux";
+import React from "react";
+import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import getCacheLoadData from "./src/hooks/onStartFetchData";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
-import { firebaseApp } from "./src/constants/Firebase";
 
 function App() {
   const isLoadingComplete = getCacheLoadData();
   const colorScheme = useColorScheme();
-
-  const dispatch = useDispatch();
-
-  // If already logged in - set user
-  useEffect(() => {
-    firebaseApp.auth().onAuthStateChanged((user) => {
-      const currentUser = {
-        id: user?.uid,
-        username: user?.email,
-        image_url: user?.photoURL,
-      };
-      if (user !== null) {
-        dispatch({ type: "LOGIN_USER", payload: currentUser });
-      }
-    });
-  }, []);
 
   // Show splash screen while data is loading
   if (!isLoadingComplete) {
@@ -39,7 +22,8 @@ function App() {
   }
 }
 
-// This exist so that we can communicate with our global state in the root - a replacement for "index.ts" if you will.
+// This exist so that we can communicate with our global state in the root
+// - A replacement for "index.ts" if you will.
 export default function AppWrapper() {
   return (
     <Provider store={store}>
--- src/hooks/onStartFetchData.ts
+++ src/hooks/onStartFetchData.ts
@@ -1,15 +1,33 @@
 import * as SplashScreen from 'expo-splash-screen';
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/actions"
+import { firebaseApp } from '../constants/Firebase';
 import axios from "axios";
 
 export default function getCacheLoadData() {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const dispatch = useDispatch();
 
-  // TODO: Load any resources or data that we need prior to rendering the app
+  // Attempt to authenticate user
   React.useLayoutEffect(() => {
-    async function loadData() {
+
+    function loadData() {
       try {
         SplashScreen.preventAutoHideAsync();
+
+        // If authentication passes, setUser
+        // with currentUser
+        firebaseApp.auth().onAuthStateChanged((user) => {
+          const currentUser = {
+            id: user?.uid,
+            username: user?.email,
+            image_url: user?.photoURL,
+          };
+          if (user !== null) {
+            dispatch(setUser(currentUser));
+          }
+        });
 
         // TODO: 
         // - Get cached data or,
